841ca53cc53aa3388b7540daf4fd7f025f29a35e
Yago Gomes Da Silva <99800161+YagoGomesDaSilva@users.noreply.github.com>
2024-05-06 21:13:31 -0300

    Modificação do EstoqueService

--- src/main/java/com/ufrn/imd/ecommerce/controllers/EstoqueController.java
+++ src/main/java/com/ufrn/imd/ecommerce/controllers/EstoqueController.java
@@ -3,7 +3,11 @@ package com.ufrn.imd.ecommerce.controllers;
 import com.ufrn.imd.ecommerce.models.entidades.Estoque;
 import com.ufrn.imd.ecommerce.models.entidades.Produto;
 import com.ufrn.imd.ecommerce.services.EstoqueService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/estoque")
@@ -15,23 +19,23 @@ public class EstoqueController {
         this.estoqueService = estoqueService;
     }
 
-    @PutMapping("/{anuncianteId}")
-    public Estoque atualizarEstoque(@PathVariable Long anuncianteId, @RequestBody Produto produto, @RequestParam int quantidade) {
-        return estoqueService.updateEstoque(anuncianteId, produto, quantidade);
+    @PostMapping("/{anuncianteId}")
+    public Estoque createEstoque(@PathVariable Long anuncianteId, @RequestBody Produto produto) {
+        return estoqueService.createEstoque(anuncianteId, produto).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
     }
 
-    @PostMapping("/{anuncianteId}")
-    public Estoque criarEstoque(@PathVariable Long anuncianteId, @RequestBody Produto produto) {
-        return estoqueService.createEstoque(anuncianteId, produto);
+    @PutMapping("/{anuncianteId}")
+    public Estoque updateEstoque(@PathVariable Long anuncianteId, @RequestBody Produto produto, @RequestParam int quantidade) {
+        return estoqueService.updateEstoque(anuncianteId, produto, quantidade).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
     }
 
     @DeleteMapping("/{anuncianteId}")
-    public Estoque deletarEstoque(@PathVariable Long anuncianteId, @RequestBody Produto produto) {
-        return estoqueService.deleteEstoque(anuncianteId, produto);
+    public Estoque deleteEstoque(@PathVariable Long anuncianteId, @RequestBody Produto produto) {
+        return estoqueService.deleteEstoque(anuncianteId, produto).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
     }
 
     @GetMapping("/{anuncianteId}")
-    public Estoque encontrarEstoque(@PathVariable Long anuncianteId, @RequestBody Produto produto) {
-        return estoqueService.findEstoque(anuncianteId, produto);
+    public Estoque findEstoque(@PathVariable Long anuncianteId, @RequestBody Produto produto) {
+        return estoqueService.findEstoque(anuncianteId, produto).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
     }
 }
--- src/main/java/com/ufrn/imd/ecommerce/services/EstoqueService.java
+++ src/main/java/com/ufrn/imd/ecommerce/services/EstoqueService.java
@@ -31,6 +31,86 @@ public class EstoqueService {
         this.anuncianteRepository = anuncianteRepository;
     }
 
+    public Optional<Estoque> updateEstoque(Long anuncianteId, Produto produto, int quantidade) {
+        Optional<Anunciante> anuncianteOpc = anuncianteRepository.findAnuncianteByIdWithDetails(anuncianteId);
+        Anunciante anunciante = anuncianteOpc.orElseThrow(() -> new UsuarioExCustom(UsuarioEnumEx.USUARIO_NAO_ENCONTRADO));
+
+        List<Estoque> estoques = anunciante.getEstoques();
+
+        Estoque estoque = verificaExistenciaProduto(estoques, produto.getId());
+        if(estoque == null){
+            throw new ProdutoExCustom(ProdutoEnumEx.PRODUTO_NAO_ENCONTRADO);
+        }
+
+        int novaQuantidade = estoque.getQuantidade() + quantidade;
+        if (novaQuantidade < 0) {
+            throw new EstoqueExCustom(EstoqueEnumEx.QUANTIDADE_INVALIDA);
+        }
+        estoque.setQuantidade(novaQuantidade);
+        return Optional.of(estoqueRepository.save(estoque));
+    }
+
+    public Optional<Estoque> createEstoque(Long anuncianteId, Produto produto){
+
+        Optional<Anunciante> anuncianteOpc = anuncianteRepository.findAnuncianteByIdWithDetails(anuncianteId);
+        Anunciante anunciante = anuncianteOpc.orElseThrow(() -> new UsuarioExCustom(UsuarioEnumEx.USUARIO_NAO_ENCONTRADO));
+
+        List<Estoque> estoques = anunciante.getEstoques();
+
+        if(verificaExistenciaProduto(estoques, produto.getId()) != null){
+            throw new EstoqueExCustom(EstoqueEnumEx.ESTOQUE_JA_EXISTENTE);
+        }
+        produtoRepository.save(produto);
+        Estoque estoque = new Estoque();
+        estoque.setAnunciante(anunciante);
+        estoque.setProduto(produto);
+        return Optional.of(estoqueRepository.save(estoque));
+    }
+
+    public Optional<Estoque> deleteEstoque(Long anuncianteId, Produto produto){
+        Optional<Anunciante> anuncianteOpc = anuncianteRepository.findAnuncianteByIdWithDetails(anuncianteId);
+        Anunciante anunciante = anuncianteOpc.orElseThrow(() -> new UsuarioExCustom(UsuarioEnumEx.USUARIO_NAO_ENCONTRADO));
+
+        List<Estoque> estoques = anunciante.getEstoques();
+
+        Estoque estoque = verificaExistenciaProduto(estoques, produto.getId());
+
+        if(estoque == null){
+            throw new EstoqueExCustom(EstoqueEnumEx.ESTOQUE_NAO_ENCONTRADO);
+        }
+
+        produtoRepository.delete(produto);
+        estoque.setAnunciante(null);
+        estoqueRepository.save(estoque);
+        estoqueRepository.delete(estoque);
+        return Optional.of(estoque);
+    }
+
+    public Optional<Estoque> findEstoque(Long anuncianteId, Produto produto){
+        Optional<Anunciante> anuncianteOpc = anuncianteRepository.findAnuncianteByIdWithDetails(anuncianteId);
+        Anunciante anunciante = anuncianteOpc.orElseThrow(() -> new UsuarioExCustom(UsuarioEnumEx.USUARIO_NAO_ENCONTRADO));
+
+        List<Estoque> estoques = anunciante.getEstoques();
+
+        Estoque estoque = verificaExistenciaProduto(estoques, produto.getId());
+
+        if(estoque == null){
+            throw new EstoqueExCustom(EstoqueEnumEx.ESTOQUE_NAO_ENCONTRADO);
+        }
+
+        return Optional.of(estoque);
+    }
+
+    private Estoque verificaExistenciaProduto( List<Estoque> estoques, Long idProcurado) {
+        for (Estoque estoque : estoques) {
+            if (estoque.getProduto().getId().equals(idProcurado)) {
+                return estoque;
+            }
+        }
+        return null;
+    }
+
+/*
     public Estoque updateEstoque(Long anuncianteId, Produto produto, int quantidade) {
         Optional<Anunciante> anuncianteOpc = anuncianteRepository.findAnuncianteByIdWithDetails(anuncianteId);
         Anunciante anunciante = anuncianteOpc.orElseThrow(() -> new UsuarioExCustom(UsuarioEnumEx.USUARIO_NAO_ENCONTRADO));
@@ -100,15 +180,8 @@ public class EstoqueService {
 
         return estoque;
     }
+    */
 
-    private Estoque verificaExistenciaProduto( List<Estoque> estoques, Long idProcurado) {
-        for (Estoque estoque : estoques) {
-            if (estoque.getProduto().getId().equals(idProcurado)) {
-                return estoque;
-            }
-        }
-        return null;
-    }
 
 }
 
